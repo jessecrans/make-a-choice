daeeb84ba80581fc465aa3801c69bc5d310c5881
jessecrans <jesse.crans@live.nl>
2025-08-17 00:32:35 +0200

    finished functionailty of elimination

---
+++ src/components/Bracket.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    let entries: string[] = [
+        "apple",
+        "banana",
+        "pear",
+        "lemon",
+        "strawberry",
+        "melon",
+        "mango",
+        "pineapple"
+    ]
+
+    /* 
+        How to make a bracket dynamically:
+
+        every piece consists of two entries or one if odd number of entries
+        we can do one sided bracket pretty easily
+        just make random pairs call it round one
+        reduce the pairs until there are 7 or 8 pairs
+        then change to quarter finals, semi-final, final
+    */
+</script>
+
+<p>TEST</p>
